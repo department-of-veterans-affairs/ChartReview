848c67f227fea44e1bb6d028c50374227dc0d01d
Brad <brad.adams@hsc.utah.edu>
2015-01-23 13:31:53 -0700

    IE cared about an extra comma...no other browser did

--- web-app/app/model/CRModelBase.js
+++ web-app/app/model/CRModelBase.js
@@ -1,5 +1,5 @@
 Ext.define('CR.app.model.CRModelBase', {
-    extend: 'Ext.data.Model',
+    extend: 'Ext.data.Model'
 //    override: 'Ext.data.schema.Schema',
     // This configures the default schema because we don't assign an "id":
     // In Extjs5, the raw reader generated field went away.  This provides a transition time behavior. TBD Do not depend on this.
