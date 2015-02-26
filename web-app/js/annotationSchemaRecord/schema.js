5bd6087bf5827525ebd4c725f68276f9b361f2f4
Ryan Cornia <ryan.cornia@utah.edu>
2015-02-26 08:47:52 -0700

    schema WIP - Fixed date format when writing the XML from angular screen

--- web-app/js/annotationSchemaRecord/schema.js
+++ web-app/js/annotationSchemaRecord/schema.js
@@ -256,13 +256,13 @@ function createXml(model) {
         if (!attribute.minDate || 0 === attribute.minDate) {
             xml += "<minDate>0000-12-30T07:00:00Z</minDate>";
         } else {
-            xml += "<minDate>" +  attribute.minDate.getFullYear() + "-" + (attribute.minDate.getMonth() + 1) + "-" + attribute.minDate.getDate() + "T07:00:00Z</minDate>";
+            xml += "<minDate>" +  attribute.minDate.getFullYear() + "-" + ("0" + (attribute.minDate.getMonth() + 1)).slice(-2) + "-" + ("0" + attribute.minDate.getDate()).slice(-2)  + "T07:00:00Z</minDate>";
         }
 
         if (!attribute.maxDate || 0 === attribute.maxDate) {
             xml += "<maxDate>9999-01-01T07:00:00</maxDate>";
         } else {
-            xml += "<maxDate>" +  attribute.maxDate.getFullYear() + "-" + (attribute.maxDate.getMonth() + 1) + "-" + attribute.maxDate.getDate() + "T07:00:00Z</maxDate>";
+            xml += "<maxDate>" +  attribute.maxDate.getFullYear() + "-" + ("0" + (attribute.maxDate.getMonth() + 1)).slice(-2) + "-" + ("0" + attribute.maxDate.getDate()).slice(-2) + "T07:00:00Z</maxDate>";
         }
         xml += "<attributeDefOptionDefs>";
 
