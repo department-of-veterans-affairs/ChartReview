f2696d9e49ce6c62ade092119f62817ba8f2f9de
Brad <brad.adams@hsc.utah.edu>
2015-03-27 10:12:04 -0600

    test page text/html

--- grails-app/controllers/chartreview/TestController.groovy
+++ grails-app/controllers/chartreview/TestController.groovy
@@ -1,34 +1,27 @@
 package chartreview
 
-import gov.va.vinci.chartreview.ProcessVariablesEnum
-import gov.va.vinci.chartreview.SummaryTableDTO
 import gov.va.vinci.chartreview.model.Project
 import gov.va.vinci.chartreview.model.Role
-import grails.plugin.gson.converters.GSON
 
 class TestController {
     def projectService;
 
     def up() {
-//        response.setContentType("text/html");
-//        response.setCharacterEncoding("utf-8");
-// as GSON is making the response content type application/json which IE tries to download instead of display....Works on other browsers and solar winds.
         try
         {
             List<Project> userProjects = projectService.projectsUserIsAssignedTo("admin", Role.findByName("ROLE_ADMIN")).sort{it.name};
             if(userProjects.size() > 0)
             {
-                render([status:"OK"] as GSON)
+                render("OK")
             }
             else
             {
-                render([status:"ERROR"] as GSON)
+                render("ERROR")
             }
         }
         catch(Exception e)
         {
-            render([status:"ERROR"] as GSON)
-
+            render("ERROR")
         }
     }
 }
